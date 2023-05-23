be662991b4ba88face363916d2b9f5781de8cfc6
Vadim N <nvvpost@yandex.ru>
2023-05-23 23:12:12 +0700

    bubble_sort, pass_swap, pair_swap

--- bubble_sort.v
+++ bubble_sort.v
@@ -5,6 +5,7 @@
 
 */
 
+
 module testbench();
 
 	parameter DIM = 4;    // длина массива
@@ -45,7 +46,12 @@ module testbench();
 endmodule
 	
 
+/*
 
+	Пузырьковая сортировка, основанная только на комбинаторной логике,
+	без применения синхронной логики, т.е. регистров.
+
+*/
 module bubble_sort
 	#(
 		parameter DIM = 4,   // длина массива
@@ -55,45 +61,87 @@ module bubble_sort
 		input [DIM*WIDTH-1:0] prand, // исходный упакованный неотсортированный массив
 		output [DIM*WIDTH-1:0] pord  // упакованный в битовый вектор результат сортировки
 	);
-
-	wire [WIDTH-1:0] arand [DIM-1:0]; // исходный неотсортированный массив
-	wire [WIDTH-1:0] aord [DIM-1:0];  // результат сортировки
 	
+	pass_swap #(DIM, WIDTH) pass0 (DIM-1, prand, pord); 
 
-	generate
+	// genvar i;
+	// generate
+		// for (i = 0; i < DIM; i = i + 1) begin
+			// assign prand[(WIDTH + 1) * i - 1 : WIDTH * i] = arand[i];
+			// assign aord[i] = pord[(WIDTH + 1) * i - 1 : WIDTH * i];
+		// end
+	// endgenerate
+	// assign
 
 endmodule
 
+
+/*
+
+	Один проход пузырьковой сортировки.	Самый тяжёлый элемент тонет до 
+	заданного уровня.
+
+*/
 module pass_swap
 	#(
-		parameter DIM = 4,
-		parameter WIDTH = 8
+		parameter DIM = 4,   // длина массива
+		parameter WIDTH = 8  // размерность элемента
 	)
 	(
-		input [N-1:0] a [DIM-1:0],
-		output [N-1:0] a [DIM-1:0]
+		input integer level, // индекс последнего элемента в обрабатываемой части массива
+		input  [DIM*WIDTH-1:0] prand, // исходный упакованный неотсортированный массив
+		output [DIM*WIDTH-1:0] pord   // упакованный в битовый вектор результат сортировки
 	);
+	
+	// распаковка и упаковка входного и выходного векторов данных для
+	// передачи отдельных элементов в модуль pair_swap
+	wire [WIDTH-1:0] arand [DIM-1:0]; // исходный неотсортированный массив
+	wire [WIDTH-1:0] aord  [DIM-1:0]; // результат сортировки
+	genvar i;
+	generate
+		for (i = 0; i <= level; i = i + 1) begin
+			assign arand[i] = prand[(WIDTH + 1) * i - 1 : WIDTH * i];
+			assign pord[(WIDTH + 1) * i - 1 : WIDTH * i] = aord[i];
+		end
+	endgenerate
 
-    pair_swap #(.N(N)) [N-1:0] swap();
-	assign
-		q1 = d1 > d2 ? d2 : d1,
-		q2 = d1 > d2 ? d1 : d2;
 
+
+	// wire 
+	// genvar i,j;
+	// generate
+		// for (i = 0; i < level; i = i + 1) begin
+			
+			// for (j = 0; j < i; j = j + 1)
+			// pair_swap #(WIDTH) pair();
+			// assign prand[(WIDTH + 1) * i - 1 : WIDTH * i] = arand[i];
+			// assign aord[i] = pord[(WIDTH + 1) * i - 1 : WIDTH * i];
+		// end
+	// endgenerate
 endmodule
 
+
+/*
+
+	Уппорядочивает два значения
+
+*/
 module pair_swap
 	#(
-		parameter N = 8
+		parameter WIDTH = 8  // размерность элемента
 	)
 	(
-		d1[N-1:0],
-		d2[N-1:0],
-		q1[N-1:0],
-		q2[N-1:0]
+		// не уппорядоченная пара значений
+		input rand1[WIDTH-1:0],
+		input rand2[WIDTH-1:0],
+		
+		// уппорядоченная пара значений
+		output ord1[WIDTH-1:0],
+		output ord2[WIDTH-1:0]
 	);
 
 	assign
-		q1 = d1 > d2 ? d2 : d1,
-		q2 = d1 > d2 ? d1 : d2;
+		ord1 = rand1 > rand2 ? rand2 : rand1,
+		ord2 = rand1 > rand2 ? rand1 : rand2;
 
 endmodule
