a1ba22dd9895baae161b8b80622aee79149d7291
Vadim N <nvvpost@yandex.ru>
2023-05-30 05:47:54 +0700

    pass_swap testbench warnning

--- code/pass_swap/t.v
+++ code/pass_swap/t.v
@@ -2,9 +2,9 @@
 
 	pass_swap module testbench
 	
-	С задержкой по времени инициализирует исходный массив сначала
-	набором случайных чисел, потом результатом одного из экземпляров
-	тестируемого модуля.
+	С задержкой по времени инициализирует исходный массив для первого
+	экземпляра тестируемого модуля набором случайных чисел, для каждого 
+	следующего экземпляра - результатом предидущего.
 
 */
 module testbench;
@@ -14,16 +14,8 @@ module testbench;
 
 	genvar i, j;
 	
-//	generate
-//		// упаковка входного массива
-//		for (j = 0; j < DIM; j = j + 1) begin : pk // Pack
-//			assign pak_less[WIDTH * (j + 1) - 1 : WIDTH * j] = ran[j];
-//		end
-//	endgenerate
-
-	
 	generate
-		for (i = 0; i < DIM; i = i + 1) begin : p // Pass
+		for (i = 1; i < DIM; i = i + 1) begin : p // Pass
 			reg  [WIDTH-1:0] ran [DIM-1:0]; // входной массив (Random)
 			wire [WIDTH-1:0] ord [DIM-1:0]; // выходной массив (Ordered)
 			wire [DIM * WIDTH - 1 : 0] pak_less; // Packed, Less ordered
@@ -31,13 +23,13 @@ module testbench;
 
 			// упаковка/распаковка входного/выходного массива
 			for (j = 0; j < DIM; j = j + 1) begin : upk // Unpack
-			assign pak_less[WIDTH * (j + 1) - 1 : WIDTH * j] = ran[j];
+				assign pak_less[WIDTH * (j + 1) - 1 : WIDTH * j] = ran[j];
 				assign ord[j] = pak_more[WIDTH * (j + 1) - 1 : WIDTH * j];
 			end
 
 			pass_swap #(DIM, i, WIDTH) pass (pak_less, pak_more);
 			
-			if (i == 0) begin
+			if (i == 1) begin
 				task check_pass;
 					integer k;
 					begin
@@ -45,7 +37,9 @@ module testbench;
 						for (k = 0; k < DIM; k = k + 1)
 							ran[k] = $random % (1 << DIM);
 						#30;
-						$display("pass %d:", i)
+						$display("pass %2d:", i);
+						for (k = 0; k < DIM; k = k + 1)
+							$display("%d: %d - %d", k, ran[k], ord[k]);
 					end
 				endtask
 			end
@@ -55,43 +49,30 @@ module testbench;
 					begin
 						// инициализация массива предидущими результатами
 						for (k = 0; k < DIM; k = k + 1)
-							ran[k] = ord[k];
+							ran[k] = p[i-1].ord[k];
+						#30;
+						$display("pass %2d:", i);
+						for (k = 0; k < DIM; k = k + 1)
+							$display("%d: %d - %d", k, ran[k], ord[k]);
 					end
 				endtask
-
-		// упаковка входного массива
-		for (j = 0; j < DIM; j = j + 1) begin : pk // Pack
+			end
 		end
 	endgenerate
 
-	integer k, l;
 	initial begin
 		#10;
-		
-		p[0].check_pass;
-		p[1].check_pass;
-		p[2].check_pass;
-		p[3].check_pass;
-		p[4].check_pass;
-		p[5].check_pass;
-		p[6].check_pass;
-		p[7].check_pass;
-
-
-		// вывод результатов и подготовка входных данных соответствующих следующей интерации
-		k = 1;
-		forever begin : prn
-			#30;
-			$display("iteration: %d", k);
-			for (l = 0; l < DIM; l = l + 1)
-				$display("%d: %d - %d", l, ran[l], p[k].ord[l]);
-			if (k >= DIM)
-				disable prn;
-			for (l = 0; l < DIM; l = l + 1)
-				ran[l] = p[k].ord[l];
-			k = k + 1;
-		end
-
+		/*
+			genblk2 пририсовывает Modelsim.
+			Это можно увидеть на вкадке sim.
+		*/
+		p[1].genblk2.check_pass;
+		p[2].genblk2.check_pass;
+		p[3].genblk2.check_pass;
+		p[4].genblk2.check_pass;
+		p[5].genblk2.check_pass;
+		p[6].genblk2.check_pass;
+		p[7].genblk2.check_pass;
 		$stop;
 	end
 endmodule
